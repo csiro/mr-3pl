5d430a9becd73550e132de1975482448b0409a72
Jensen, Murray (Mineral Resources, Clayton) <Murray.Jensen@csiro.au>
2024-03-21 17:18:00 +1100

    yet another cleanup of the tools subdir

---
+++ sources/tools/info2c/prog_fpga.h
@@ -0,0 +1,20 @@
+
+// interface "SR" in space "reg" 
+typedef uint8_t fpga_SR_t;
+const unsigned fpga_SR_addr = 2;
+static fpga_SR_t
+fpga_SR_read() {
+    fpga_SR_t v;
+    comms_read(0, fpga_SR_addr, 0, 1, 0, 0, (char *)&v);
+    return(v);
+}
+
+// interface "SW" in space "reg" 
+typedef uint8_t fpga_SW_t;
+const unsigned fpga_SW_addr = 4;
+static void
+fpga_SW_write(fpga_SW_t val) {
+    comms_write(0, fpga_SW_addr, 0, 1, (char *)&val);
+}
+
+const unsigned fpga_interrupts = 0;
--- sources/tools/zynq/setup.py
+++ sources/tools/zynq/setup.py
@@ -1,17 +1,14 @@
 from distutils.core import setup, Extension
 import os
 
-def main():
-    instdir = os.environ['HOME'] + '/install/3pl'
+instdir = os.environ['HOME'] + '/install/3pl'
 
-    module = Extension('fpga_read_write',
-        include_dirs = [instdir + '/include'],
-        library_dirs = [instdir + '/lib/python3'],
-        libraries = ['z'],
-        sources = ['fpga_py_extension.c', 'fpga.c'])
+module = Extension(
+    'fpga_read_write',
+    include_dirs=[instdir + '/include'],
+    library_dirs=[instdir + '/lib/python3'],
+    libraries=['z'],
+    sources=['fpga_py_extension.c', 'fpga.c'],
+)
 
-    setup (name = 'fpga_read_write',
-           ext_modules = [module])
-
-if (__name__ == "__main__"):
-  main()
+setup(name='fpga_read_write', ext_modules=[module])
